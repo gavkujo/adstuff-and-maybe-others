4e874122f57e1debe7a45b73290b2c632131aa42
gavkujo <83745829+gavkujo@users.noreply.github.com>
2021-09-24 18:57:04 +0530

    comments

--- timepass.ino
+++ timepass.ino
@@ -1,3 +1,7 @@
+// the assignment was to connect 3 LEDs. red, green, blue.
+// blink red 5 times, green 10 times and blue 15 times
+// this is the most tedious way of doing this. im pretty sure there is some other much easier and shorter way of achieving the same output.
+
 int del = 100;
 
 void setup() {
