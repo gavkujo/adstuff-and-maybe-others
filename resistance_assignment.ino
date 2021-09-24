b5df376e7c1c7746b5d5f8151598222458e9f838
gavkujo <83745829+gavkujo@users.noreply.github.com>
2021-09-24 19:01:25 +0530

    1023 error thing

--- resistance_assignment.ino
+++ resistance_assignment.ino
@@ -13,6 +13,8 @@ void setup() {
 
 void loop() {
   val = analogRead(readPin);
+  // im dividing the read value by 5/1023 cuz like for some reason it reads 5V as 1023 which is way off.
+  // also no i did not set it up wrong. this thing has happened to everyone
   V2 = (5./1023.)*val;
   Serial.println(V2);
   delay(del);
